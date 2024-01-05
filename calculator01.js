f4af049481b1d40baa68f14dedb1b4d4670e6dcb
jaimzh <155784286+jaimzh@users.noreply.github.com>
2024-01-06 00:39:44 +0100

    Add files via upload

---
+++ calculator01.js
@@ -0,0 +1,27 @@
+const display = document.getElementById("display");
+
+//showing whatever button that was clicked on the display screen
+function showInDisplay(input){
+    display.value = display.value + input;
+}
+
+//clear display
+function clearDisplay(){
+    display.value = '';
+}
+
+// delete one character (backspace)
+function deButton(){
+    display.value
+}
+
+function calculate() {
+    var display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+        display.classList.add('errorx'); 
+    }
+}
+
